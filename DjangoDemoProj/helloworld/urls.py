49614e0a2d55bd622a70f6524986ee55e2d2e06d
shichao1986 <unique1986@163.com>
2018-12-25 18:06:00 +0800

    render test

--- DjangoDemoProj/helloworld/urls.py
+++ DjangoDemoProj/helloworld/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls import url
 from . import views as hello_views
 
 urlpatterns = [
-    url(r'^hello/index/(?P<name>[\w]+/(?P<age>[\d]+))', hello_views.index),
+    url(r'^hello/index/(?P<name>[\w]+)/(?P<age>[\d]+)', hello_views.index, name='hello'),
+    url(r'^hello/index2/(?P<name>[\w]+)/(?P<age>[\d]+)', hello_views.index2, name='hello2'),
+    url(r'^hello/index_render/(?P<name>[\w]+)/(?P<age>[\d]+)', hello_views.index_render, name='hello_render'),
 ]
--- DjangoDemoProj/helloworld/views.py
+++ DjangoDemoProj/helloworld/views.py
@@ -2,8 +2,21 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 
 def index(request, name, age):
     return HttpResponse('Hello world! {} is {} year(s) old.'.format(name, age))
+
+def index2(request, name, age):
+    return render('helloworld/index.html', dict(name=name, age=age))
+
+def index_render(request, name, age):
+    return render('helloworld/index_render.html', dict(name=name, age=age))
+
+def index_old(request, name, age):
+    pass
+
+def index3(request, name, age):
+    return
