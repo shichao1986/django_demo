ba7e4722884e292463bb8f54dc141381f3adcb6f
shichao1986 <unique1986@163.com>
2018-12-26 14:52:46 +0800

    多对多关系表

--- DjangoDemoProj/helloworld/models.py
+++ DjangoDemoProj/helloworld/models.py
@@ -17,7 +17,9 @@ class Person(models.Model):
     name = models.CharField('姓名', max_length=32, unique=False, help_text='姓名')
     age = models.IntegerField('年龄', null=False, default=0)
     school = models.ForeignKey(School, on_delete=models.SET_NULL, related_name='persons', verbose_name='学校',
-                               help_text='指向学校的外键', default=None)
+                               help_text='指向学校的外键', default=None, null=True, blank=True)
+    books = models.ManyToManyField(Book, through='PersonBookRelation', through_fields=('person', 'book'), blank=True,
+                                   null=True, related_name='person_relations', verbose_name='书籍')
 
     def __str__(self):
         return self.name
@@ -36,3 +38,16 @@ class Book(models.Model):
     class Meta:
         app_label = 'helloworld'
         db_table = 'book'
+
+class PersonBookRelation(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='relations', verbose_name='人员',
+                               help_text='人员')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='relations', verbose_name='书籍',
+                             help_text='书籍')
+
+    def __str__(self):
+        return '{}-{}'.format(self.person.name, self.book.name)
+
+    class Meta:
+        app_label = 'helloworld'
+        db_table = 'person_book_relation'
