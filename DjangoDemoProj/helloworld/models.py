6d7e0125f5e737bd92828d3d74e71085c1b20d4f
shichao1986 <unique1986@163.com>
2018-12-26 13:35:59 +0800

    添加外键表

--- DjangoDemoProj/helloworld/models.py
+++ DjangoDemoProj/helloworld/models.py
@@ -2,9 +2,22 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField('名称', max_length=64, unique=True, help_text='学校名称')
+    address = models.TextField('地址', default='', help_text='学校地址')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        app_label = 'helloworld'
+        db_table = 'school'
+
 class Person(models.Model):
     name = models.CharField('姓名', max_length=32, unique=False, help_text='姓名')
     age = models.IntegerField('年龄', null=False, default=0)
+    school = models.ForeignKey(School, on_delete=models.SET_NULL, related_name='persons', verbose_name='学校',
+                               help_text='指向学校的外键', default=None)
 
     def __str__(self):
         return self.name
