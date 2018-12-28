b37d23d2db1a828a07fe170a67a1de74fdc4b231
shichao1986 <unique1986@163.com>
2018-12-28 17:25:21 +0800

    增加djagno后台的使用示例

--- DjangoDemoProj/helloworld/admin.py
+++ DjangoDemoProj/helloworld/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
 from . import models
 
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'favorates', 'school__name')
+    search_fields = ('name', 'age', 'favorates', 'school__name')
+    list_filter = ('school__name')
+
+    # 可以根据需求限制get请求的qset
+    def get_queryset(self, request):
+        qs = super(PersonAdmin, self).get_queryset(request)
+        # 可以根据需求限制get请求的qset
+        # TODO
+        return qs
+
+    def save_model(self, request, obj, form, change):
+        # 可以根据需求在保存前增加操作
+        # TODO
+        super(PersonAdmin, self).save_model(request, obj, form, change)
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super(PersonAdmin, self).get_search_results(request, queryset, search_term)
+        # 可以根据需求增加搜索条件
+        # TODO
+        return queryset, use_distinct
+
+    def delete_model(self, request, obj):
+        # 可以根据需求增加删除前的操作
+        # TODO
+        super(PersonAdmin, self).delete_model(request, obj)
+
+
 # Register your models here.
-admin.site.register(models.Person)
+admin.site.register(models.Person, PersonAdmin)
 admin.site.register(models.Book)
 admin.site.register(models.School)
 admin.site.register(models.PersonBookRelation)
